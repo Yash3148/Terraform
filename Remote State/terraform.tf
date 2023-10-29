6b829883254f2da0946ad47c21d65fe1726f5ec2
Yash <yashsingh3148@gmail.com>
2023-10-29 14:02:46 +0530

    Terraform

---
+++ Remote State/terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+    key = "terraform.tfstate"
+    region = "us-east-1"
+    bucket = "remote-state"
+    endpoint = "http://172.16.238.105:9000"
+    force_path_style = true
+
+
+    skip_credentials_validation = true
+
+    skip_metadata_api_check = true
+    skip_region_validation = true
+  }
+}
+
+# Backend configuration which defines exactly where and how operations are performed, where state snapshots are stored, etc.
